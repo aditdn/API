670a68e3cea98ff171389b685671810070c1c992
BSI80197 <BSI80197@BSINB23F012.bsidomain.com>
2023-09-11 15:17:47 +0700

    Create test case CDA_2.9

---
+++ Object Repository/CDA_2.9-Check response with birthdate less than 7 years age.rs
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>CDA_2.9-Check response with birthdate less than 7 years age</name>
+   <tag></tag>
+   <elementGuidId>69e376c8-2e8e-42f8-a6c6-ef40f2ad5f73</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <authorizationRequest>
+      <authorizationInfo>
+         <entry>
+            <key>bearerToken</key>
+            <value>eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiNjE0MzNjMTI5ZjMzYjBmMGQ4OWZlNmFjM2Y5NTc5NjQ1ZDNhMTE1ZjE2NmI4YTRhNTRkZTM5OTM0YmY3OGEwNzFkZTZkYzhlOWEzN2ZmNTgiLCJpYXQiOjE2OTMyOTUwOTkuODc5MzQzLCJuYmYiOjE2OTMyOTUwOTkuODc5MzQ3LCJleHAiOjE3MjQ5MTc0OTkuODc1NjMyLCJzdWIiOiI2NCIsInNjb3BlcyI6W119.bftQnfYk6x7Gvavhnl--XHuPPH5JZkokoBtgDyAmlREggrBKSX8I-QBaZfOe_qq_U_6SvKJxiBgjIS030TofJ1FXE_BrwAmfjLDpB0DuxfAB2e4ZWSTi_jdfz3vxiCgXTU6h-YZZKcNBw4036vgA6WosHB5kOQIAO7fcyUMckWSH3EkCW-nIpuKvRG7NlAkz8A3cvZOoeWgxSMPQWnj2kxivKZu29OdPgwbzUEYuq3o9xnk4xgHqVCWN2m1eFzilMpUvz5RJ1jnMw_smQxjkJ0LKxxBDxweKYUN4DhJMOQLGch9KGud10NGQNSOEDkjWQWrA-tAXXa0irbvLzvLA43Xa8GvYOLvfqk0xP7EIW8oPEJSDhOa_9gW8aGbu4QTQMsMAR4h3bmV7e-nJSPeA6OnEloUqX5NlN1RBVBP24QH_tE2U2OJQphmmczTNpk_Q8E_Shfl2Jq2r6Sw2jfZz8A9Wpe7n7aQtYDA7UFFPT8CP3E48Ht7pzMbIWOQp_j3JOw45KfxYg7OLwleYg-Iv-A7J_VRyUT2_7FVjHCL-gxzgUXdBi0uAuOx5H-1l8t89NZtXYonphgG28ozid4yQDOgGDr8c3-KQ-9JwuKl9bVwaj-xpwxeVvDg9HsJLZR3l4LtLXpIecOftjoYJY7j2vMNzsXfjgRbbbNbAY8MOkYI</value>
+         </entry>
+      </authorizationInfo>
+      <authorizationType>Bearer</authorizationType>
+   </authorizationRequest>
+   <autoUpdateContent>true</autoUpdateContent>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;contentType&quot;: &quot;multipart/form-data&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;,
+  &quot;parameters&quot;: [
+    {
+      &quot;name&quot;: &quot;name&quot;,
+      &quot;value&quot;: &quot;update name&quot;,
+      &quot;type&quot;: &quot;Text&quot;,
+      &quot;contentType&quot;: &quot;&quot;
+    },
+    {
+      &quot;name&quot;: &quot;whatsapp&quot;,
+      &quot;value&quot;: &quot;1234567890&quot;,
+      &quot;type&quot;: &quot;Text&quot;,
+      &quot;contentType&quot;: &quot;&quot;
+    },
+    {
+      &quot;name&quot;: &quot;birth_date&quot;,
+      &quot;value&quot;: &quot;2016-12-31&quot;,
+      &quot;type&quot;: &quot;Text&quot;,
+      &quot;contentType&quot;: &quot;&quot;
+    },
+    {
+      &quot;name&quot;: &quot;photo&quot;,
+      &quot;value&quot;: &quot;&quot;,
+      &quot;type&quot;: &quot;Text&quot;,
+      &quot;contentType&quot;: &quot;&quot;
+    },
+    {
+      &quot;name&quot;: &quot;bio&quot;,
+      &quot;value&quot;: &quot;Software Dev&quot;,
+      &quot;type&quot;: &quot;Text&quot;,
+      &quot;contentType&quot;: &quot;&quot;
+    },
+    {
+      &quot;name&quot;: &quot;position&quot;,
+      &quot;value&quot;: &quot;mobile dev&quot;,
+      &quot;type&quot;: &quot;Text&quot;,
+      &quot;contentType&quot;: &quot;&quot;
+    }
+  ]
+}</httpBodyContent>
+   <httpBodyType>form-data</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>multipart/form-data</value>
+      <webElementGuid>b9ef47e4-5c8d-40d2-b29d-8d1af5b97d48</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiNjE0MzNjMTI5ZjMzYjBmMGQ4OWZlNmFjM2Y5NTc5NjQ1ZDNhMTE1ZjE2NmI4YTRhNTRkZTM5OTM0YmY3OGEwNzFkZTZkYzhlOWEzN2ZmNTgiLCJpYXQiOjE2OTMyOTUwOTkuODc5MzQzLCJuYmYiOjE2OTMyOTUwOTkuODc5MzQ3LCJleHAiOjE3MjQ5MTc0OTkuODc1NjMyLCJzdWIiOiI2NCIsInNjb3BlcyI6W119.bftQnfYk6x7Gvavhnl--XHuPPH5JZkokoBtgDyAmlREggrBKSX8I-QBaZfOe_qq_U_6SvKJxiBgjIS030TofJ1FXE_BrwAmfjLDpB0DuxfAB2e4ZWSTi_jdfz3vxiCgXTU6h-YZZKcNBw4036vgA6WosHB5kOQIAO7fcyUMckWSH3EkCW-nIpuKvRG7NlAkz8A3cvZOoeWgxSMPQWnj2kxivKZu29OdPgwbzUEYuq3o9xnk4xgHqVCWN2m1eFzilMpUvz5RJ1jnMw_smQxjkJ0LKxxBDxweKYUN4DhJMOQLGch9KGud10NGQNSOEDkjWQWrA-tAXXa0irbvLzvLA43Xa8GvYOLvfqk0xP7EIW8oPEJSDhOa_9gW8aGbu4QTQMsMAR4h3bmV7e-nJSPeA6OnEloUqX5NlN1RBVBP24QH_tE2U2OJQphmmczTNpk_Q8E_Shfl2Jq2r6Sw2jfZz8A9Wpe7n7aQtYDA7UFFPT8CP3E48Ht7pzMbIWOQp_j3JOw45KfxYg7OLwleYg-Iv-A7J_VRyUT2_7FVjHCL-gxzgUXdBi0uAuOx5H-1l8t89NZtXYonphgG28ozid4yQDOgGDr8c3-KQ-9JwuKl9bVwaj-xpwxeVvDg9HsJLZR3l4LtLXpIecOftjoYJY7j2vMNzsXfjgRbbbNbAY8MOkYI</value>
+      <webElementGuid>9a124e72-629b-4cdc-bfb9-bff6f4768293</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>8.6.6</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>https://demo-app.online/api/updateprofile</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
